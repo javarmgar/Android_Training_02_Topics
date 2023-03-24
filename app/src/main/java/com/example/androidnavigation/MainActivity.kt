728366ee33b65d709df9d93452524e9c34d8c20b
Javier Armenta <ja0822@dexcom.com>
2023-03-24 14:26:39 -0700

    Getting started with navigation adding navigation dependencies along with safe args dependency and implementing NavGraph, NavHostFragment and NavController without using safeArgs

--- app/src/main/java/com/example/androidnavigation/MainActivity.kt
+++ app/src/main/java/com/example/androidnavigation/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.androidnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        findViewById<Button>(R.id.btn_fragment_1).setOnClickListener {
+            navController.navigate(R.id.fragmentOne)
+        }
+
+        findViewById<Button>(R.id.btn_fragment_2).setOnClickListener {
+            navController.navigate(R.id.fragmentTwo)
+        }
     }
+
 }
