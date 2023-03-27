c48c372f249626533f5570e19f5f46058fc52375
Javier Armenta <ja0822@dexcom.com>
2023-03-26 18:32:49 -0700

    using  implicit destinations by using implicit intents where as always it being searched by using catoegory data and action manifest intenfilters attributes on the destination navigation component however i have some doubts regarding the way to infer the activity destination. Also using Dynamic arguments   where additional information is sent by using the URL for BROWSABLE intents by using dataPattern attribute where the URL has some placeholders and the arguments are supplied in the argument tag

--- app/src/main/java/com/example/androidnavigation/SecondActivity.kt
+++ app/src/main/java/com/example/androidnavigation/SecondActivity.kt
@@ -3,6 +3,8 @@ package com.example.androidnavigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.core.os.bundleOf
+
 import androidx.navigation.fragment.NavHostFragment
 
 class SecondActivity : AppCompatActivity() {
@@ -13,16 +15,20 @@ class SecondActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-
-        /*findViewById<Button>(R.id.btn_dialog_fragment).setOnClickListener {
-            with(navController){
-                if(navController.currentDestination?.id == R.id.secondActivity){
-                 navigate(R.id.myDialogFragment)
-                }
-            }
-        }*/
         findViewById<Button>(R.id.btn_dialog_fragment).setOnClickListener {
             MyDialogFragment().show(supportFragmentManager,MyDialogFragment.TAG)
+        }
+
+        findViewById<Button>(R.id.btn_open_act_img).setOnClickListener {
+            navController.navigate(R.id.url_chatgpt)
+        }
+
+        findViewById<Button>(R.id.btn_open_youtube).setOnClickListener {
+            navController.navigate(R.id.activity_url_youtube,
+                bundleOf(
+                    "videoId" to "hoJ4qxcXz5w"
+                )
+            )
         }
     }
 }
